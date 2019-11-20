20a35acde4272904b4c410a927be9570461b60e5
Arif Hosan <hosan.arif0@gmail.com>
2019-11-20 17:23:51 +0100

    minor fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   restApi: 'https://per86r2mvj.execute-api.ap-southeast-1.amazonaws.com/dev/api/',
   judgeApi: 'https://api.judge0.com/',
-  baseUrl: 'http://ide.arifhosan.com/',
+  baseUrl: 'http://web-ide.arifhosan.com/',
 };
